36d33e83c1490c7a756786d6c457fbb8a8d8fab4
pifragile <pigu@bluewin.ch>
2021-09-16 21:52:50 +0200

    initial site

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,86 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="pug">
+h1 Welcome to pifragile's NFTs!
+a.twitter-follow-button(href='https://twitter.com/pifragile?ref_src=twsrc%5Etfw' data-show-count='false') Follow @pifragile
+h3
+  | I am a programmer and NFT artist working with different flavors of generative art!
+  br
+  | If you need help with any technical issues concerning generative art, mint automation, smart contracts, metamask, etc... DM me on twitter. I have a Master&apos;s Degree in Computer Science and experience in those areas!
+  br
+  br
+  | Check out my projects below (reload page for new NFTs and click images to view on OpenSea)
+.nft-container
+  NFTSeries(
+        v-for='nft in nfts'
+        :nftName='nft.nft_name'
+        :nftUrl='nft.nft_url'
+        :imageUrl='nft.image_url'
+        :seriesName='nft.series_name'
+        :seriesUrl='nft.series_url'
+        :seriesDescription='nft.series_description'
+      )
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NFTSeries from './components/NFTSeries.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NFTSeries
+  },
+  data() {
+    return {
+      nfts: []
+    }
+  },
+
+    methods: {
+    async getData() {
+      const response = await axios.get('https://space.pigu.ch/pifragile/get-nft-series/')
+      this.nfts = response.data
+      console.log(this.nfts)
+    },
+    async getData2() {
+      try {
+        const response = await this.$http.get(
+          'https://space.pigu.ch/pifragile/get-nft-series/'
+        );
+        // JSON responses are automatically parsed.
+        this.nfts = response.data;
+        console.log(this.nfts);
+      } catch (error) {
+        console.log(error);
+      }
+  },
+
+  },
+
+  created() {
+    this.getData();
+  },
+
+
+  mounted() {
+    document.title = 'pifragile\'s NFTs'
+    let twitterScript = document.createElement('script')
+    twitterScript.setAttribute('src', 'https://platform.twitter.com/widgets.js')
+    document.head.appendChild(twitterScript)
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css?family=VT323');
+body {
+  font-family: 'VT323', sans-serif;
+  background-color: #fff70f;
 }
+
+.nft-container {
+  display: flex; /* or inline-flex */
+  flex-wrap: wrap;
+}
+
 </style>
---
+++ src/components/NFTSeries.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+
+.nft-box#d_csc
+  h2
+    a(:href='seriesUrl') {{seriesName}}
+  .nft-description
+    p {{seriesDescription}}
+  h4 {{nftName}}
+  a#a_csc(:href='nftUrl')
+    img.nft(:src='imageUrl')
+</template>
+
+<script>
+export default {
+  name: 'NFTSeries',
+  props: {
+    nftName: String,
+    nftUrl: String,
+    imageUrl: String,
+    seriesName: String,
+    seriesUrl: String,
+    seriesDescription: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+img.nft {
+  height: 400px;
+  width: 400px;
+}
+
+.nft-box {
+  padding: 10px;
+  width: 420px;
+  height: 600px;
+}
+
+.nft-description {
+  height: 50px;
+}
+</style>
